47c5d3a6bbb78b56851edc1293cfbb005f730ba8
Olas8 <aleksander.muszynski@gmail.com>
2020-05-18 11:11:58 +0200

    remove /.idea from repo

--- app/src/main/java/com/example/dziennikazja/ui/HomeFragment.java
+++ app/src/main/java/com/example/dziennikazja/ui/HomeFragment.java
@@ -1,51 +1,69 @@
 package com.example.dziennikazja.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.dziennikazja.R;
-import com.example.dziennikazja.databinding.FragmentMemberInfoDetailsBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeFragment extends Fragment {
-    private FragmentMemberInfoDetailsBinding binding;
+    private static final String TAG = "HomeFragment";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        /*binding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_home, container, false);
 
-        Member member = new Member("BoA");
-        member.lastName = "Kim";
-        binding.setMember(member);
-
-        return binding.getRoot();*/
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        //binding = null;
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        /*ImageButton btnTest = view.findViewById(R.id.test_button);
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = db.getReference();
+        myRef = myRef.child("members/mmeiqi/lastName");
+
+        Button btnTest = view.findViewById(R.id.test_button);
+        DatabaseReference finalMyRef = myRef;
         btnTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), binding.getMember().firstName +" " +binding.getMember().lastName, Toast.LENGTH_SHORT).show();
+                finalMyRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //Getting the string value of that node
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d(TAG, "onDataChange: " + value);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Log.e(TAG, "onCancelled: Something went wrong! Error:" + databaseError.getMessage());
+
+                    }
+                });
+
             }
-        });*/
+        });
     }
 
 }
